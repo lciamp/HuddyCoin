a7c72f4bdacb9d840e2060276d52da1eae57fff1
lciamp <l.ciamp@yahoo.com>
2021-11-14 18:23:18 -0500

    cleanup & main example

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
 type BlockChain struct {
-	blocks []Block
+	blocks []*Block
 }
 
 type Block struct {
@@ -16,7 +17,7 @@ type Block struct {
 }
 
 func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.data, b.PrevHash}, []byte{})
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
@@ -27,9 +28,9 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string){
-	prevBloc := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBloc.Hash)
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, new)
 }
 
@@ -42,5 +43,15 @@ func InitBlockChain() *BlockChain {
 }
 
 func main() {
+	chain := InitBlockChain()
 
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
