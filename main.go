787bbe2d897afd8704520226a35efba97113ab3e
lciamp <l.ciamp@yahoo.com>
2021-11-14 16:28:40 -0500

    add hash function

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
 }
 
 func main() {
