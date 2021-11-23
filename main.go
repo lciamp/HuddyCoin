e5ed588c90def3ddd9258bfba279dc881f5addd4
lciamp <l.ciamp@yahoo.com>
2021-11-23 17:43:09 -0500

    add proof of work

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"go-blockchain/blockchain"
+	"strconv"
 )
 
 func main() {
@@ -16,5 +17,9 @@ func main() {
 		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
