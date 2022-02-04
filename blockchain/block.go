a64b683404aae4119d64b2a86e6bb9032888c986
lciamp <l.ciamp@yahoo.com>
2022-02-03 19:30:29 -0500

    db

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -25,7 +31,39 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-// Genesis generates the first block of the blockchain
+// Genesis method on Block struct that generates the first block of the blockchain
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
+}
+
+// Serialize method on Block struct that outputs a slice of bytes
+func (b *Block) Serialize() []byte {
+	// result
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
+}
+
+// Deserialize method on Block takes serialized data and out puts a pointer to a block
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error){
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,19 +1,85 @@
 package blockchain
 
+import (
+	"fmt"
+	"github.com/dgraph-io/badger"
+)
+
+// path to DB
+const (
+	dbPath = "./tmp/blocks"
+)
+
 // BlockChain is a basic structure for a blockchain
 type BlockChain struct {
-	Blocks []*Block
+	LashHash []byte
+	Database *badger.DB
 }
 
-// InitBlockChain creates a new blockchain
 func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+	var lastHash []byte
+
+	opts := badger.DefaultOptions
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	// open db connection
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	// passing in closure
+	// check if there is a blockchain, if not create it
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.Value()
+			return err
+		}
+	})
+
+	// handle any error from the if/else
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db}
+	return &blockchain
 }
 
 // AddBlock adds a block to the blockchain
 func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.Value()
+
+		return err
+	})
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
 }
 
